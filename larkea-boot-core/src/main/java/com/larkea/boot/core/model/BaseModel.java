aa01ea851ea154b965b37f2802ce179496332d27
Le Wang <lewang.dev@gmail.com>
2020-08-26 18:13:22 +0800

    0.2.20082601

--- larkea-boot-core/src/main/java/com/larkea/boot/core/model/BaseModel.java
+++ larkea-boot-core/src/main/java/com/larkea/boot/core/model/BaseModel.java
@@ -1,9 +1,11 @@
 package com.larkea.boot.core.model;
 
+import java.io.Serializable;
+
 import com.larkea.boot.core.data.DataSupport;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public abstract class BaseModel<D> extends DataSupport<D> {
+public abstract class BaseModel<D> extends DataSupport<D> implements Serializable {
 
 }
